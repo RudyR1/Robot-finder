652e02c0c90dbc984d816330e2505d1654d183ca
RudyR1 <rudy.rano972@gmail.com>
2023-04-09 12:08:08 +0200

    RobotFinder-question1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import React from 'react';
 import './App.css';
 
 import Robots from './data/data';
+//import RobotCard from './components/RobotCard';
 
 function App() {
   console.log(Robots)
   return (
     <div className="App">
       <h1>Robot Finder</h1>
+        {Robots.map((robot) => {return (<div key={robot.id}>
+          <h2>{robot.name} {robot.username}</h2>
+          <p>{robot.phone}</p>
+          <p>{robot.email}</p>
+          <p>{robot.website}</p>
+          </div>)
+      
+        })}
     </div>
   );
 }
